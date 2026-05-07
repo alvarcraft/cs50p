de4d7bd7266e651785a4b6337792b6964743032d
alvarcraft <alvar.kujala@icloud.com>
2026-05-07 18:34:25 +0300

    Add files via upload

---
+++ Training/Week 0/compare.py
@@ -0,0 +1,12 @@
+x = int(input("enter x digit "))
+y = int(input("enter y digit "))
+if x != y:
+    print("x is not equal to y")
+#if x < y
+    #print("x is less than y")
+# else if
+#elif x > y:
+    #print("y is less than x")
+#youve asked all other possible outcomes, just end it here with "else"
+else:
+    print("x is equal to y")
